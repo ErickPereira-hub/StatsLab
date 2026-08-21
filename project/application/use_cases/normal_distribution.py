26cebd15b8d601a58aa454f16d9894209ce5826a
erick <erick1204200100@gmail.com>
2026-08-21 01:42:03 -0300

    Normal distribution has been implemented successfully

--- project/application/use_cases/normal_distribution.py
+++ project/application/use_cases/normal_distribution.py
@@ -8,7 +8,12 @@ class NormalDistributionUseCase:
     PRECISION: int = 10000 #<--- Precision is directly proportional to the correct value of the probability
 
     @classmethod
-    def get_probability(cls, start_val: Optional[int | float], end_val: Optional[int | float], std_deviation: float, avg: float) -> float:
+    def __validate_range(cls, start, end) -> bool:
+        if start > end: return False
+        return True
+
+    @classmethod
+    def get_probability(cls, start_val: Optional[int | float], end_val: Optional[int | float], std_deviation: float, avg: float) -> float | None:
 
         #Density probability function for the ND
         density: Callable[[float | int], float] = lambda x : ( np.e ** ( ( -1 / 2 ) * ( ( x - avg ) / std_deviation ) ** 2 ) ) / ( std_deviation * sqrt(2 * np.pi) )
@@ -34,7 +39,7 @@ class NormalDistributionUseCase:
                     b = end_val
                 ).get_integral()
 
-            if end_val == 0: return 0
+            if end_val == 0: return 0.5
 
         if end_val is None:
 
@@ -54,7 +59,10 @@ class NormalDistributionUseCase:
                     b = start_val
                     ).get_integral()
 
-            if start_val == 0: return 0
+            if start_val == 0: return 0.5
+
+        if start_val == end_val: return 0 #<--- Identical inputs must return ZERO
+        if not cls.__validate_range(start_val, end_val): return None #<--- Validating range. If None is returned, it means that the input is wrong such that the starting point is bigger than the ending point.
 
         #Acquiring probability of fiding the value between start_val and end_val where the extremes are limitted
         prob: float = IntegralUseCase(
